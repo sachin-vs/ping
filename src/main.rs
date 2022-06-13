4e40ac9f7e012d9b8ae944f51123e42eb77d47d9
sachin-vs <sachin.mi20@iiitmk.ac.in>
2022-06-13 17:04:15 +0530

    updated comments

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,51 @@ use libp2p::{identity, Multiaddr, PeerId};
 use std::error::Error;
 
 #[async_std::main]
+/*
+/////////////Peer Identity/////////////////////
+local_key--> Public key of peer generated from ED25519
+loca_peer_id --> Private key created from public key
+*/
 async fn main() -> Result<(), Box<dyn Error>> {
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
     println!("Local peer id: {:?}", local_peer_id);
+    /*
+    /////////Transport using TCP and noise for encryption//////////
+    defines how to send bytes on the network
+    using development_transport function
 
+    */
     let transport = libp2p::development_transport(local_key).await?;
 
-    // Create a ping network behaviour.
-    //
-    // For illustrative purposes, the ping protocol is configured to
-    // keep the connection alive, so a continuous sequence of pings
-    // can be observed.
+    /*
+    ///////Behaviour/////////
+    Define what bytes to send
+    ping protocol sends 32 bytes of random data.
+    */
     let behaviour = Ping::new(PingConfig::new().with_keep_alive(true));
-
+    /*
+    ///////////Swarm//////////
+    Connect Transport and Network behaviour
+    pass commands from NetworkBehaviour and Transport and Events from Transport to NetworkBehaviour
+    */
     let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
 
-    // Tell the swarm to listen on all interfaces and a random, OS-assigned
-    // port.
+    /*
+
+    ///////Listening to all interface at defined port/////
+    Using swarm.listen
+
+    */
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    // Dial the peer identified by the multi-address given as the second
-    // command-line argument, if any.
+    // Check second line argument in the command
     if let Some(addr) = std::env::args().nth(1) {
         let remote: Multiaddr = addr.parse()?;
         swarm.dial(remote)?;
         println!("Dialed {}", addr)
     }
-
+    // Drive swarm in a loop- listen to incoming connections
     loop {
         match swarm.select_next_some().await {
             SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {:?}", address),
